8c98d8bc0ff77c2ced0dc276855ed786de876788
shabnam.kh <shabnam.khodadadi@gmail.com>
2021-07-23 12:58:19 +0430

    Add main name

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from builtins import open
 from kitchen import Kitchen
 
 
-with open('input.txt') as reader:
-    i=0
-    for line in reader.readlines():
-        line = line.rstrip()
-        if i==0: # first line is
-            kitchen = Kitchen(capacity=line)
-        else:
-            print(kitchen.process_order(order=line))
-        i+=1
+def main():
+    with open('input.txt') as reader:
+        i=0
+        for line in reader.readlines():
+            line = line.rstrip()
+            if i==0: # first line is kitchen capacity
+                kitchen = Kitchen(capacity=line)
+            else:
+                print(kitchen.process_order(order=line))
+            i+=1
 
-    print("R1", ",", "TOTAL", ",", kitchen.get_total_process_time())
-    print("R1", ",",  "INVENTORY", ",", kitchen.get_inventories_state())
+        print("R1", ",", "TOTAL", ",", kitchen.get_total_process_time())
+        print("R1", ",",  "INVENTORY", ",", kitchen.get_inventories_state())
+
+
+if __name__ == '__main__':
+    main()
